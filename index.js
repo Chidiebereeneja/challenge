64480154ed060654171c89320686dd1757490aeb
Chidiebere Eneja <chidiebereravy@gmail.com>
2024-12-15 02:57:33 +0100

    Test

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const form = document.querySelector("main");
-const btn = document.querySelector("div");
+const btn = document.querySelector("button");
 
 form.addEventListener("mousemove", (e) => {
 	const rect = btn.getBoundingClientRect();
 	const xMove = e.clientX < rect.left + rect.width / 2 ? 30 : -30;
 	const yMove = e.clientY < rect.top + rect.height / 2 ? 30 : -30;
 	btn.style.transform = `translate(${xMove}px, ${yMove}px)`;
+
+	btn.style.boxShadow = `0px 4px 10px rgba(216, 216, 216)`;
 });
